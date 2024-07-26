3eea610500a55dd0f9dc82875229407cf5cc26ab
EEB12 <rayhan.taqara@yahoo.com>
2024-07-26 20:22:41 +0700

    update transaction route

--- routes/API/V1/index.js
+++ routes/API/V1/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const USER_ROUTER = require("./user");
 const ACCOUNT_ROUTER = require("./account");
-// const TRANSACTION_ROUTER = require("./transaction");
-// const BORROWING_ROUTER = require("./borrowing.route");
+const TRANSACTION_ROUTER = require("./transaction");
+
 
 router.use("/user", USER_ROUTER);
 
 router.use("/account", ACCOUNT_ROUTER);
-// router.use("/transaction", TRANSACTION_ROUTER);
+router.use("/transaction", TRANSACTION_ROUTER);
 module.exports = router;
